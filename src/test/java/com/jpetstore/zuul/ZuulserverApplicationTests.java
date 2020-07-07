0126c9bdb113ee2592bc1b310bd9a59822f0145b
funnyjoy <funnyjoy@P00074430-002.CNS.LGCNS.COM>
2020-07-07 09:53:19 +0900

    Init commit

---
+++ src/test/java/com/jpetstore/zuul/ZuulserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpetstore.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
